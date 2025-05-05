92041bef08973734334dad4ad8dc8dbe31716052
cdryampi <cdryampi@gmail.com>
2025-05-05 11:39:01 +0200

    probando el despligue de los proyectos.

---
+++ proyectos_memes/cv_nihilista/vite.config.js
@@ -0,0 +1,8 @@
+// proyectos_memes/cv_nihilista/vite.config.js
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  base: "/proyectos_memes/cv_nihilista/",
+  plugins: [vue()],
+});
