4dae70b794fd4b6e25ba1fdce2993b0a10188a31
cdryampi <cdryampi@gmail.com>
2025-05-05 13:28:25 +0200

    refactor: update deployment workflow for proyectos_memes with improved project structure and naming

--- proyectos_memes/articulo_nihilista/vite.config.js
+++ proyectos_memes/articulo_nihilista/vite.config.js
@@ -1,9 +1,9 @@
-// proyectos_memes/cv_nihilista/vite.config.js
+// proyectos_memes/proyectos_memes/vite.config.js
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/proyectos_memes/articulo_nihilista/",
+  base: "/proyectos_memes/proyectos_memes/articulo_nihilista/",
   plugins: [vue(), tailwindcss()],
 });
